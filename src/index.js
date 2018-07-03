cb1c98f250a6c6e61c3906c90b9110f460a6a1fb
blevm <mjblevins7@gmail.com>
2018-07-03 15:28:58 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Code Goes Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SomeComponent from './SomeComponent';
+
+let array = ['blue', 'green', 'yellow'];
+
+ReactDOM.render(
+  <SomeComponent>
+    {array.map(color=> <div>{color}</div>)}
+  </SomeComponent>,
+  document.getElementById('root')
+)
